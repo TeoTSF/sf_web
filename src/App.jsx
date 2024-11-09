3b096d2eabbbe983ccfb69e8b9791e270a4fbbd6
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2024-11-09 14:51:38 -0400

    blog creation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import ProtectedRoutes from "./components/ProtectecRoutes";
 import Home from "./pages/Home";
 import NavBar from "./components/navbar/NavBar";
+import BlogItemView from "./pages/blog/BlogItemView";
+import Blog from "./pages/blog/Blog";
 
 function App() {
   
@@ -12,6 +14,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/post/:id" element={<BlogItemView />} />
         <Route element={<ProtectedRoutes />}>
         </Route>
       </Routes>
---
+++ src/pages/blog/utils/FilterComponent.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import {
+  useMediaQuery,
+  IconButton,
+  Menu,
+  MenuItem,
+  Button,
+} from "@mui/material";
+import FilterListIcon from "@mui/icons-material/FilterList";
+
+const blogFilters = [
+  { id: "", category: "Todos"},
+  { id: 1, category: "Trading" },
+  { id: 2, category: "Análisis" },
+  { id: 3, category: "Resultados" },
+  { id: 4, category: "Clases" },
+  { id: 5, category: "Gestión" },
+];
+
+const FilterComponent = () => {
+  const [selectedFilter, setSelectedFilter] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isMobile = useMediaQuery("(max-width:600px)");
+
+  useEffect(() => {
+    if (selectedFilter) {
+      const fetchData = async () => {
+        const response = await fetch(
+          `https://api.example.com/data?filter=${selectedFilter}`
+        );
+        const data = await response.json();
+        console.log("Datos filtrados:", data);
+      };
+      fetchData();
+    }
+  }, [selectedFilter]);
+
+  const handleFilterSelect = (category) => {
+    setSelectedFilter(category);
+    setAnchorEl(null);
+  };
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className="filter-container">
+      {isMobile ? (
+        <>
+          <IconButton onClick={handleMenuOpen} className="filter-icon">
+            <FilterListIcon fontSize="large" />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            {blogFilters.map((filter) => (
+              <MenuItem
+                key={filter.id}
+                onClick={() => handleFilterSelect(filter.category)}
+                className={`menu-item ${
+                  selectedFilter === filter.category ? "selected" : ""
+                }`}
+              >
+                {filter.category}
+              </MenuItem>
+            ))}
+          </Menu>
+        </>
+      ) : (
+        <div className="desktop-filters">
+          {blogFilters.map((filter) => (
+            <Button
+              key={filter.id}
+              onClick={() => handleFilterSelect(filter.category)}
+              className={`desktop-filter-button ${
+                selectedFilter === filter.category ? "selected" : ""
+              }`}
+            >
+              {filter.category}
+            </Button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FilterComponent;
