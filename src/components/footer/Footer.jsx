3b2fb2264b12167e71445ab73ecd55fc5509a174
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2024-11-08 20:02:21 -0400

    footer update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useContext } from "react";
+import "./footer.css";
+import MainContext from "../../context/MainContext";
 
 const Footer = () => {
-    return (
-        <div>
-            
+  const { handleScrollToTop } = useContext(MainContext);
+  return (
+    <>
+      <div className="footer_container flex row wrap">
+        <div className="footer_info full_w full_h">
+
         </div>
-    );
+      </div>
+      <div className="footer_last_section full-w flex jf-c al-c relative autoM">
+        <p>© 2024 Trading sin fronteras. Todos los derechos reservados.</p>
+        <button
+          className="to_top_btn btn_app absolute"
+          onClick={handleScrollToTop}
+        >
+          <i className='bx bx-chevrons-up bx-md' />
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
