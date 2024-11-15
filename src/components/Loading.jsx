f81ed3933899a97a87b00e4764ee41eeb59e9cf9
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2024-11-14 20:37:22 -0400

    blog integrate

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,21 +1,14 @@
-import React from 'react';
-import { CircularProgress, Box, Typography } from '@mui/material';
+import React from "react";
+import { CircularProgress, Box, Typography } from "@mui/material";
+import Curtain from "./generals/Curtain";
 
-export default function Loading({ message = "Cargando..." }) {
+export default function Loading({ loading, message = "Cargando..." }) {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-      }}
-    >
-      <CircularProgress color="primary" />
+    <Curtain open={loading}>
+      <CircularProgress color="primary" /><br /><br />
       <Typography variant="h6" sx={{ marginTop: 2 }}>
         {message}
       </Typography>
-    </Box>
+    </Curtain>
   );
 }
